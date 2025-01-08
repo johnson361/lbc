3c5e2c6e98a299769a2c98b8278a9c1ebfad021e
Robert Johnson <rjohnson@talentlogic.com>
2025-01-07 22:03:49 -0600

    fixes

--- lbc.sql
+++ lbc.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 04, 2025 at 09:09 AM
--- Server version: 8.3.0
--- PHP Version: 8.1.28
+-- Generation Time: Jan 08, 2025 at 04:03 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.1.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -57,6 +57,9 @@ CREATE TABLE IF NOT EXISTS `offerings` (
   `service_date` date DEFAULT NULL,
   `user_id` int NOT NULL,
   `serial_no` int NOT NULL DEFAULT '0',
+  `is_check` tinyint(1) NOT NULL DEFAULT '0',
+  `check_no` int DEFAULT NULL,
+  `check_amount` int DEFAULT NULL,
   `denomination_2000` int NOT NULL DEFAULT '0',
   `denomination_500` int DEFAULT '0',
   `denomination_200` int DEFAULT '0',
@@ -77,68 +80,19 @@ CREATE TABLE IF NOT EXISTS `offerings` (
   PRIMARY KEY (`id`),
   KEY `service_id` (`service_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=161 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `offerings`
 --
 
-INSERT INTO `offerings` (`id`, `service_id`, `service_date`, `user_id`, `serial_no`, `denomination_2000`, `denomination_500`, `denomination_200`, `denomination_100`, `denomination_50`, `denomination_20_notes`, `denomination_20_coins`, `denomination_10_notes`, `denomination_10_coins`, `denomination_5_notes`, `denomination_5_coins`, `denomination_2_notes`, `denomination_2_coins`, `denomination_1_notes`, `denomination_1_coins`, `created_at`) VALUES
-(104, 21, '2024-12-31', 91, 1, 0, 19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:52:26'),
-(105, 23, '2024-12-31', 93, 1, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:54:07'),
-(106, 23, '2024-12-31', 94, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:54:21'),
-(107, 23, '2024-12-31', 95, 3, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:54:36'),
-(108, 23, '2024-12-31', 96, 4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:54:50'),
-(109, 23, '2024-12-31', 97, 5, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:55:23'),
-(110, 23, '2024-12-31', 92, 6, 0, 0, 0, 6, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:55:59'),
-(111, 23, '2024-12-31', 98, 7, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:56:25'),
-(112, 23, '2024-12-31', 99, 8, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:56:46'),
-(113, 23, '2024-12-31', 100, 9, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 09:56:57'),
-(114, 24, '2024-12-31', 101, 1, 0, 47, 20, 169, 62, 150, 1, 82, 2, 0, 17, 0, 38, 0, 28, '2025-01-01 10:21:28'),
-(115, 21, '2025-01-01', 102, 1, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:37:59'),
-(116, 21, '2025-01-01', 103, 2, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:38:20'),
-(117, 21, '2025-01-01', 104, 3, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:38:36'),
-(118, 21, '2025-01-01', 105, 4, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:38:55'),
-(119, 21, '2025-01-01', 106, 5, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:39:09'),
-(120, 21, '2025-01-01', 107, 6, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:39:22'),
-(121, 21, '2025-01-01', 108, 7, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:39:37'),
-(122, 21, '2025-01-01', 109, 8, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:39:50'),
-(123, 21, '2025-01-01', 110, 9, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:40:00'),
-(124, 21, '2025-01-01', 111, 10, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:40:10'),
-(125, 21, '2025-01-01', 112, 11, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:40:27'),
-(126, 21, '2025-01-01', 113, 12, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:40:59'),
-(127, 21, '2025-01-01', 114, 13, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:41:04'),
-(128, 21, '2025-01-01', 115, 14, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:41:29'),
-(129, 21, '2025-01-01', 117, 15, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:41:46'),
-(130, 21, '2025-01-01', 116, 16, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:43:02'),
-(131, 52, '2025-01-01', 101, 1, 0, 1, 0, 4, 1, 9, 0, 4, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:46:12'),
-(132, 23, '2025-01-01', 118, 1, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:48:09'),
-(133, 23, '2025-01-01', 119, 2, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:48:24'),
-(134, 23, '2025-01-01', 120, 3, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:48:34'),
-(135, 23, '2025-01-01', 121, 4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:48:44'),
-(136, 23, '2025-01-01', 122, 5, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:48:58'),
-(137, 23, '2025-01-01', 123, 6, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:49:08'),
-(138, 23, '2025-01-01', 124, 7, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:49:48'),
-(139, 23, '2025-01-01', 125, 8, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:49:59'),
-(140, 23, '2025-01-01', 126, 9, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:50:09'),
-(141, 23, '2025-01-01', 127, 10, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:50:20'),
-(142, 23, '2025-01-01', 128, 11, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:50:33'),
-(143, 23, '2025-01-01', 129, 12, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:51:02'),
-(144, 23, '2025-01-01', 130, 13, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:51:19'),
-(145, 23, '2025-01-01', 131, 14, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:51:39'),
-(146, 23, '2025-01-01', 132, 15, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:51:58'),
-(147, 23, '2025-01-01', 133, 16, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:52:08'),
-(148, 23, '2025-01-01', 134, 17, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:52:19'),
-(149, 23, '2025-01-01', 135, 18, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:52:46'),
-(150, 23, '2025-01-01', 136, 19, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:53:08'),
-(151, 23, '2025-01-01', 100, 20, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:53:34'),
-(152, 23, '2025-01-01', 137, 21, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:53:53'),
-(153, 23, '2025-01-01', 138, 22, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:54:21'),
-(154, 23, '2025-01-01', 139, 23, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:54:58'),
-(155, 23, '2025-01-01', 140, 24, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:55:11'),
-(156, 23, '2025-01-01', 141, 25, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:55:27'),
-(157, 23, '2025-01-01', 142, 26, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-01 10:55:39'),
-(158, 24, '2025-01-01', 101, 1, 0, 42, 20, 162, 75, 123, 1, 75, 4, 0, 7, 0, 9, 0, 7, '2025-01-01 10:57:56');
+INSERT INTO `offerings` (`id`, `service_id`, `service_date`, `user_id`, `serial_no`, `is_check`, `check_no`, `check_amount`, `denomination_2000`, `denomination_500`, `denomination_200`, `denomination_100`, `denomination_50`, `denomination_20_notes`, `denomination_20_coins`, `denomination_10_notes`, `denomination_10_coins`, `denomination_5_notes`, `denomination_5_coins`, `denomination_2_notes`, `denomination_2_coins`, `denomination_1_notes`, `denomination_1_coins`, `created_at`) VALUES
+(217, 24, '2025-01-06', 165, 1, 1, NULL, NULL, 0, 44, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-06 16:29:50'),
+(218, 24, '2025-01-06', 167, 2, 1, NULL, NULL, 0, 44, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-06 16:30:16'),
+(219, 24, '2025-01-06', 168, 3, 1, NULL, NULL, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-06 16:30:40'),
+(220, 24, '2025-01-06', 169, 4, 1, NULL, NULL, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-06 16:30:50'),
+(221, 24, '2025-01-06', 170, 5, 0, NULL, NULL, 0, 3, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-06 16:30:56'),
+(222, 24, '2025-01-06', 171, 6, 0, NULL, NULL, 0, 7, 8, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2025-01-06 16:31:14');
 
 -- --------------------------------------------------------
 
@@ -208,9 +162,7 @@ INSERT INTO `services` (`id`, `language_id`, `offering_type_id`, `service_slot`,
 (43, 1, 3, '7 pm', '2024-12-17 03:45:02'),
 (44, 1, 4, '7 pm', '2024-12-26 04:14:40'),
 (50, NULL, 8, NULL, '2024-12-23 10:12:51'),
-(51, NULL, 7, NULL, '2024-12-23 10:12:10'),
-(52, NULL, 5, NULL, '2024-12-20 06:43:34'),
-(53, NULL, 6, NULL, '2024-12-20 06:43:42');
+(51, NULL, 7, NULL, '2024-12-23 10:12:10');
 
 -- --------------------------------------------------------
 
@@ -229,7 +181,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=146 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
@@ -288,7 +240,32 @@ INSERT INTO `users` (`id`, `name`, `email`, `phone`, `phone2`, `created_at`) VAL
 (139, 'G PAVAN AND LATHA', NULL, '8142770660', '', '2025-01-01 10:54:58'),
 (140, 'JAYA KUMARI', NULL, '7569893597', '', '2025-01-01 10:55:11'),
 (141, 'M SAUDHAMANI W/O JAIPAL', NULL, '9866887553', '', '2025-01-01 10:55:27'),
-(142, 'T NISHANTH S/O T AMBEDKAR', NULL, '9700123836', '', '2025-01-01 10:55:39');
+(142, 'T NISHANTH S/O T AMBEDKAR', NULL, '9700123836', '', '2025-01-01 10:55:39'),
+(147, 'TEST1', NULL, NULL, '', '2025-01-06 14:14:07'),
+(148, 'TEST2', NULL, NULL, '', '2025-01-06 14:14:44'),
+(149, 'TEST3', NULL, NULL, '', '2025-01-06 14:14:56'),
+(150, 'TEST4', NULL, NULL, '', '2025-01-06 14:17:17'),
+(151, 'TEST5', NULL, NULL, '', '2025-01-06 14:20:04'),
+(152, 'TEST6', NULL, NULL, '', '2025-01-06 14:20:23'),
+(153, 'TEST7', NULL, NULL, '', '2025-01-06 14:21:20'),
+(154, 'TEST8', NULL, NULL, '', '2025-01-06 14:25:46'),
+(155, 'TEST9', NULL, NULL, '', '2025-01-06 14:26:59'),
+(156, 'TEST10', NULL, NULL, '', '2025-01-06 14:32:47'),
+(157, 'TEST11', NULL, NULL, '', '2025-01-06 14:33:03'),
+(158, 'TEST22', NULL, NULL, '', '2025-01-06 14:57:49'),
+(159, 'TEST43', NULL, NULL, '', '2025-01-06 15:13:57'),
+(160, 'TEST', NULL, NULL, '', '2025-01-06 15:16:25'),
+(161, 'TEST45345', NULL, NULL, '', '2025-01-06 15:16:30'),
+(162, 'TET3', NULL, NULL, '', '2025-01-06 15:59:25'),
+(163, 'TET2', NULL, NULL, '', '2025-01-06 16:15:27'),
+(164, 'TSEST', NULL, NULL, '', '2025-01-06 16:20:12'),
+(165, 'AAA', NULL, NULL, '', '2025-01-06 16:28:01'),
+(166, 'BBB', NULL, NULL, '', '2025-01-06 16:28:33'),
+(167, 'SSS', NULL, NULL, '', '2025-01-06 16:30:29'),
+(168, 'CCC', NULL, NULL, '', '2025-01-06 16:30:40'),
+(169, 'FFF', NULL, NULL, '', '2025-01-06 16:30:50'),
+(170, 'JJ', NULL, NULL, '', '2025-01-06 16:30:56'),
+(171, 'JJJ', NULL, NULL, '', '2025-01-06 16:31:14');
 
 -- --------------------------------------------------------
 
@@ -298,30 +275,33 @@ INSERT INTO `users` (`id`, `name`, `email`, `phone`, `phone2`, `created_at`) VAL
 --
 DROP VIEW IF EXISTS `view_detail_offerings`;
 CREATE TABLE IF NOT EXISTS `view_detail_offerings` (
-`created_at` timestamp
-,`denomination_100` int
-,`denomination_10_coins` int
-,`denomination_10_notes` int
-,`denomination_1_coins` int
-,`denomination_1_notes` int
-,`denomination_200` int
-,`denomination_2000` int
-,`denomination_20_coins` int
-,`denomination_20_notes` int
-,`denomination_2_coins` int
-,`denomination_2_notes` int
-,`denomination_50` int
-,`denomination_500` int
-,`denomination_5_coins` int
-,`denomination_5_notes` int
+`service_name` varchar(361)
 ,`full_name` varchar(100)
 ,`id` int
-,`serial_no` int
-,`service_date` date
 ,`service_id` int
-,`service_name` varchar(361)
-,`total_amount` int
+,`service_date` date
 ,`user_id` int
+,`serial_no` int
+,`is_check` tinyint(1)
+,`check_no` int
+,`check_amount` int
+,`denomination_2000` int
+,`denomination_500` int
+,`denomination_200` int
+,`denomination_100` int
+,`denomination_50` int
+,`denomination_20_notes` int
+,`denomination_20_coins` int
+,`denomination_10_notes` int
+,`denomination_10_coins` int
+,`denomination_5_notes` int
+,`denomination_5_coins` int
+,`denomination_2_notes` int
+,`denomination_2_coins` int
+,`denomination_1_notes` int
+,`denomination_1_coins` int
+,`total_amount` int
+,`created_at` timestamp
 );
 
 -- --------------------------------------------------------
@@ -332,7 +312,7 @@ CREATE TABLE IF NOT EXISTS `view_detail_offerings` (
 DROP TABLE IF EXISTS `view_detail_offerings`;
 
 DROP VIEW IF EXISTS `view_detail_offerings`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_detail_offerings`  AS SELECT concat(coalesce(`languages`.`language_name`,'N/A'),' - ',coalesce(`offering_types`.`offering_name`,'N/A'),' - ',coalesce(`services`.`service_slot`,'N/A')) AS `service_name`, `users`.`name` AS `full_name`, `offerings`.`id` AS `id`, `offerings`.`service_id` AS `service_id`, `offerings`.`service_date` AS `service_date`, `offerings`.`user_id` AS `user_id`, `offerings`.`serial_no` AS `serial_no`, `offerings`.`denomination_2000` AS `denomination_2000`, `offerings`.`denomination_500` AS `denomination_500`, `offerings`.`denomination_200` AS `denomination_200`, `offerings`.`denomination_100` AS `denomination_100`, `offerings`.`denomination_50` AS `denomination_50`, `offerings`.`denomination_20_notes` AS `denomination_20_notes`, `offerings`.`denomination_20_coins` AS `denomination_20_coins`, `offerings`.`denomination_10_notes` AS `denomination_10_notes`, `offerings`.`denomination_10_coins` AS `denomination_10_coins`, `offerings`.`denomination_5_notes` AS `denomination_5_notes`, `offerings`.`denomination_5_coins` AS `denomination_5_coins`, `offerings`.`denomination_2_notes` AS `denomination_2_notes`, `offerings`.`denomination_2_coins` AS `denomination_2_coins`, `offerings`.`denomination_1_notes` AS `denomination_1_notes`, `offerings`.`denomination_1_coins` AS `denomination_1_coins`, `offerings`.`total_amount` AS `total_amount`, `offerings`.`created_at` AS `created_at` FROM ((((`services` join `languages` on((`services`.`language_id` = `languages`.`id`))) join `offering_types` on((`services`.`offering_type_id` = `offering_types`.`id`))) join `offerings` on((`services`.`id` = `offerings`.`service_id`))) join `users` on((`offerings`.`user_id` = `users`.`id`))) ORDER BY `offerings`.`serial_no` ASC ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_detail_offerings`  AS SELECT concat(coalesce(`languages`.`language_name`,'N/A'),' - ',coalesce(`offering_types`.`offering_name`,'N/A'),' - ',coalesce(`services`.`service_slot`,'N/A')) AS `service_name`, `users`.`name` AS `full_name`, `offerings`.`id` AS `id`, `offerings`.`service_id` AS `service_id`, `offerings`.`service_date` AS `service_date`, `offerings`.`user_id` AS `user_id`, `offerings`.`serial_no` AS `serial_no`, `offerings`.`is_check` AS `is_check`, `offerings`.`check_no` AS `check_no`, `offerings`.`check_amount` AS `check_amount`, `offerings`.`denomination_2000` AS `denomination_2000`, `offerings`.`denomination_500` AS `denomination_500`, `offerings`.`denomination_200` AS `denomination_200`, `offerings`.`denomination_100` AS `denomination_100`, `offerings`.`denomination_50` AS `denomination_50`, `offerings`.`denomination_20_notes` AS `denomination_20_notes`, `offerings`.`denomination_20_coins` AS `denomination_20_coins`, `offerings`.`denomination_10_notes` AS `denomination_10_notes`, `offerings`.`denomination_10_coins` AS `denomination_10_coins`, `offerings`.`denomination_5_notes` AS `denomination_5_notes`, `offerings`.`denomination_5_coins` AS `denomination_5_coins`, `offerings`.`denomination_2_notes` AS `denomination_2_notes`, `offerings`.`denomination_2_coins` AS `denomination_2_coins`, `offerings`.`denomination_1_notes` AS `denomination_1_notes`, `offerings`.`denomination_1_coins` AS `denomination_1_coins`, `offerings`.`total_amount` AS `total_amount`, `offerings`.`created_at` AS `created_at` FROM ((((`services` join `languages` on((`services`.`language_id` = `languages`.`id`))) join `offering_types` on((`services`.`offering_type_id` = `offering_types`.`id`))) join `offerings` on((`services`.`id` = `offerings`.`service_id`))) join `users` on((`offerings`.`user_id` = `users`.`id`))) ORDER BY `offerings`.`serial_no` ASC ;
 
 --
 -- Constraints for dumped tables
