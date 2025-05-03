64d8f9b35258b4d867b0b19ae47e0cca17bcb1c7
Robert Johnson <johnson361@gmail.com>
2025-05-03 12:44:02 +0530

    fixes

--- chagnes.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `offerings` ADD `is_check` BOOLEAN NOT NULL DEFAULT FALSE AFTER `serial_no`, ADD `check_no` INT NULL DEFAULT NULL AFTER `is_check`, ADD `check_amount` INT NULL DEFAULT NULL AFTER `check_no`;
-
-ALTER TABLE `offerings` DROP `is_check`;
